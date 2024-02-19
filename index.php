d57f8a10c4501b7fb59edb274f90432adb07cf66
subedi1680 <subedi1680@gmail.com>
2024-02-19 13:36:18 +0545

    Linked database

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "ematadan";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $phone = $_POST['phone'];
+    $pass = $_POST['pass'];
+
+    $sql = "SELECT * FROM login WHERE phone='$phone' AND pass='$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        header('Location: voter.html');
+        exit;
+    } else {
+        echo "Invalid username or password";
+    }
+
+    $conn->close();
+?>
