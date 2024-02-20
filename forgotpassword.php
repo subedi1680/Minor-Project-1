03eda184f99cab13bfdaac5263a35a9ee3908fb2
SANDYYYY <sandeshlamsal8@gmail.com>
2024-02-20 20:57:01 +0545

    otp sent

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,15 +1,33 @@
 <?php
-$email = $_POST['email'];
-$otp = rand(10000, 99999);
-$to = $email;
-$from = "subedi1680@gmail.com";
-$fromName = "eMATADAN";
-$subject = "OTP verification";
-$message = "Your OTP is " . $otp;
-$header = 'From: ' . $fromName . ' <' . $from . '>';
-if (mail($to, $subject, $message, $header)) {
-    echo "Email sent successfully.";
-} else {
-    echo "Failed to send email. Please check your server configuration or try again later.";
+include('smtp/PHPMailerAutoload.php');
+$email=$_POST['email'];
+$otp=rand(10000,99999);
+echo smtp_mailer($email,' One Time Password',$otp);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 2; 
+	$mail->Username = "sandeshlamsal8@gmail.com";
+	$mail->Password = "lagw gykg idov kwyi";
+	$mail->SetFrom("sandeshlamsal8@gmail.com");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
 }
 ?>
