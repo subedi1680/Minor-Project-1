e6a9565605ae8137713595da951bf433bd0386c7
subedi1680 <subedi1680@gmail.com>
2024-04-27 21:12:11 +0545

    voter_details fixed

--- voter_details.php
+++ voter_details.php
@@ -2,12 +2,15 @@
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     if(isset($_SESSION['user_data'])) {
         $user_data = $_SESSION['user_data'];
-        echo "<div>VOTER NAME: {$user_data['fname']} {$user_data['lname']}</div>";
-        echo "<div>DOB: {$user_data['dob']}</div>";
-        echo "<div>PHONE NUMBER: {$user_data['phone']}</div>";
+        echo "<div>
+                VOTER NAME: {$user_data['fname']} {$user_data['lname']}
+                <br>
+                DOB: {$user_data['dob']}
+                <br>
+                PHONE NUMBER: {$user_data['phone']}</div>";
+        
     }
 } else {
-    // Redirect if not logged in
     header("Location: index.html");
     exit;
 }
